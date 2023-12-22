aa1577d1b9a0a46db9a54d43d25abe5e122fce7c
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-12-22 18:07:22 +0600

    added center align

--- src/pages/Move/Move.jsx
+++ src/pages/Move/Move.jsx
@@ -31,9 +31,9 @@ const Move = () => {
         const priority = data?.priority;
         const deadline = data?.deadline;
         const status = e.target.status.value;
-        const taskDetails = {title, description, email, priority, deadline, status}
+        const taskDetails = { title, description, email, priority, deadline, status }
         const res = await axiosSecure.put(`/statusChange/${id}`, taskDetails);
-        if(res?.data?.modifiedCount){
+        if (res?.data?.modifiedCount) {
             Swal.fire({
                 title: "Success!",
                 text: `You successfully moved your task to ${status}.`,
@@ -47,13 +47,15 @@ const Move = () => {
         <div>
             <h1 className="text-center font-bold text-xl md:text-2xl lg:text-3xl py-6">Please select where you want to move this task</h1>
             <form className="max-w-screen-xl mx-auto" onSubmit={handleSubmit}>
-                <select name="status" className="border w-full mt-12 mb-6 py-3 rounded-lg">
-                    <option value="todo">TODO</option>
-                    <option value="ongoing">ONGOING</option>
-                    <option value="completed">COMPLETED</option>
-                </select>
                 <div className="flex justify-center items-center">
-                    <input className="py-1.5 w-full rounded bg-[#b63327] text-white hover:text-[#b63327] hover:bg-white hover:border hover:border-[#b63327] text-xs md:text-base" type="submit" value="Move" />
+                    <select name="status" className="border w-3/4 mt-12 mb-6 py-3 rounded-lg">
+                        <option value="todo">TODO</option>
+                        <option value="ongoing">ONGOING</option>
+                        <option value="completed">COMPLETED</option>
+                    </select>
+                </div>
+                <div className="flex justify-center items-center">
+                    <input className="py-1.5 rounded w-3/4 bg-[#b63327] text-white hover:text-[#b63327] hover:bg-white hover:border hover:border-[#b63327] text-xs md:text-base" type="submit" value="Move" />
                 </div>
             </form>
         </div>
